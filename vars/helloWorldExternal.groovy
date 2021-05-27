807d3d5ca06ae1135290c5a4f6edae74b70ca789
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-27 17:08:34 +0530

    roboshop

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,4 +1,4 @@
 def call(Map config = [:]) {
     loadLinuxScript(name: 'hello-world.sh') //load file from resource library
-    sh "./hello-world.sh ${config.name} ${config.dayOfWeek}"
+    sh "sh hello-world.sh ${config.name} ${config.dayOfWeek}"
 }
