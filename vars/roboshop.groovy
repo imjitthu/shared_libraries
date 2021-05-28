af51c1760c0a1ed9bbeb39a3fa38ee32c98b70bf
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-28 10:43:31 +0530

    roboshop

--- vars/roboshop.groovy
+++ vars/roboshop.groovy
@@ -4,31 +4,71 @@ pipeline {
 
   agent any
 
-  environment {
-      tfaction = "${action}"
-  }
-
   stages {
     stage('Installing DataBases, Redis & RabbitMQ Server') {
       parallel {
         stage ('Installing Mysql') {
           steps {
-            build job: 'RoboShop_WebApp/mysql', parameters: [string(name: 'tfaction', value: "${env.tfaction}")]
+            if (action == "apply") {
+            build job: 'RoboShop_WebApp/mysql', parameters: [string(name: 'tfaction', value: "terraform apply -auto-approve")]
+            }
+            if (action == "init") {
+            build job: 'RoboShop_WebApp/mysql', parameters: [string(name: 'tfaction', value: "terraform init")]
+            }
+            if (action == "plan") {
+            build job: 'RoboShop_WebApp/mysql', parameters: [string(name: 'tfaction', value: "terrafrom plan")]
+            }
+            if (action == "destroy") {
+            build job: 'RoboShop_WebApp/mysql', parameters: [string(name: 'tfaction', value: "terraform destory -auto-approve")]
+            }
           }
         }
         stage ('Installing Redis') {
           steps {
-            build job: 'RoboShop_WebApp/redis', parameters: [string(name: 'tfaction', value: "${env.tfaction}")]
+            if (action == "apply") {
+            build job: 'RoboShop_WebApp/redis', parameters: [string(name: 'tfaction', value: "terraform apply -auto-approve")]
+            }
+            if (action == "init") {
+            build job: 'RoboShop_WebApp/redis', parameters: [string(name: 'tfaction', value: "terraform init")]
+            }
+            if (action == "plan") {
+            build job: 'RoboShop_WebApp/redis', parameters: [string(name: 'tfaction', value: "terrafrom plan")]
+            }
+            if (action == "destroy") {
+            build job: 'RoboShop_WebApp/redis', parameters: [string(name: 'tfaction', value: "terraform destory -auto-approve")]
+            }
           }
           }
         stage ('Installing Mongo') {
           steps {
-            build job: 'RoboShop_WebApp/mongo', parameters: [string(name: 'tfaction', value: "${env.tfaction}")]
+            if (action == "apply") {
+            build job: 'RoboShop_WebApp/mongo', parameters: [string(name: 'tfaction', value: "terraform apply -auto-approve")]
+            }
+            if (action == "init") {
+            build job: 'RoboShop_WebApp/mongo', parameters: [string(name: 'tfaction', value: "terraform init")]
+            }
+            if (action == "plan") {
+            build job: 'RoboShop_WebApp/mongo', parameters: [string(name: 'tfaction', value: "terrafrom plan")]
+            }
+            if (action == "destroy") {
+            build job: 'RoboShop_WebApp/mongo', parameters: [string(name: 'tfaction', value: "terraform destory -auto-approve")]
+            }
           }
         }
         stage ('Installing RabbitMQ Server') {
           steps {
-            build job: 'RoboShop_WebApp/rabbitmq', parameters: [string(name: 'tfaction', value: "${env.tfaction}")]
+            if (action == "apply") {
+            build job: 'RoboShop_WebApp/rabbitmq', parameters: [string(name: 'tfaction', value: "terraform apply -auto-approve")]
+            }
+            if (action == "init") {
+            build job: 'RoboShop_WebApp/rabbitmq', parameters: [string(name: 'tfaction', value: "terraform init")]
+            }
+            if (action == "plan") {
+            build job: 'RoboShop_WebApp/rabbitmq', parameters: [string(name: 'tfaction', value: "terrafrom plan")]
+            }
+            if (action == "destroy") {
+            build job: 'RoboShop_WebApp/rabbitmq', parameters: [string(name: 'tfaction', value: "terraform destory -auto-approve")]
+            }
           }
         }
       }
