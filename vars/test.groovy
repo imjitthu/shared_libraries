4b6fa74c4fcc7dcc51d973f7ee1026cc0c1f9bfd
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-28 10:08:06 +0530

    roboshop

--- vars/test.groovy
+++ vars/test.groovy
@@ -10,7 +10,7 @@ def call(String action) {
         stages {
             stage ('Shared Library') {
                 steps {
-                sh 'echo "action is "${env.action}"'
+                echo "action is "${env.action}"
             }
             }
             }
