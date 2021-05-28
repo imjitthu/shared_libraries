f2aa8bdd0443fd5088813a333e036bed0858e256
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-28 11:00:25 +0530

    roboshop

--- vars/roboshop.groovy
+++ vars/roboshop.groovy
@@ -79,7 +79,119 @@ pipeline {
             }
           }
         }
+        } //parallel
+        } //stage
+    stage ('Installing RoboShop Components') {
+      parallel {
+        stage ('Installing Catalogue') {
+          steps {
+            script {
+            if (action == "apply") {
+            build job: 'RoboShop_WebApp/catalogue', parameters: [string(name: 'tfaction', value: "terraform apply -auto-approve")]
+            }
+            if (action == "init") {
+            build job: 'RoboShop_WebApp/catalogue', parameters: [string(name: 'tfaction', value: "terraform init")]
+            }
+            if (action == "plan") {
+            build job: 'RoboShop_WebApp/catalogue', parameters: [string(name: 'tfaction', value: "terraform plan")]
+            }
+            if (action == "destroy") {
+            build job: 'RoboShop_WebApp/catalogue', parameters: [string(name: 'tfaction', value: "terraform destory -auto-approve")]
+            }
+            }
+          }
         }
+        stage ('Installing Cart') {
+          steps {
+            script {
+            if (action == "apply") {
+            build job: 'RoboShop_WebApp/cart', parameters: [string(name: 'tfaction', value: "terraform apply -auto-approve")]
+            }
+            if (action == "init") {
+            build job: 'RoboShop_WebApp/cart', parameters: [string(name: 'tfaction', value: "terraform init")]
+            }
+            if (action == "plan") {
+            build job: 'RoboShop_WebApp/cart', parameters: [string(name: 'tfaction', value: "terraform plan")]
+            }
+            if (action == "destroy") {
+            build job: 'RoboShop_WebApp/cart', parameters: [string(name: 'tfaction', value: "terraform destory -auto-approve")]
+            }
+            }
+          }
+        }
+        stage ('Installing user') {
+          steps {
+            script {
+            if (action == "apply") {
+            build job: 'RoboShop_WebApp/user', parameters: [string(name: 'tfaction', value: "terraform apply -auto-approve")]
+            }
+            if (action == "init") {
+            build job: 'RoboShop_WebApp/user', parameters: [string(name: 'tfaction', value: "terraform init")]
+            }
+            if (action == "plan") {
+            build job: 'RoboShop_WebApp/user', parameters: [string(name: 'tfaction', value: "terraform plan")]
+            }
+            if (action == "destroy") {
+            build job: 'RoboShop_WebApp/user', parameters: [string(name: 'tfaction', value: "terraform destory -auto-approve")]
+            }
+            }
+          }
+        }
+        stage ('Installing Payment') {
+          steps {
+            script {
+            if (action == "apply") {
+            build job: 'RoboShop_WebApp/payment', parameters: [string(name: 'tfaction', value: "terraform apply -auto-approve")]
+            }
+            if (action == "init") {
+            build job: 'RoboShop_WebApp/payment', parameters: [string(name: 'tfaction', value: "terraform init")]
+            }
+            if (action == "plan") {
+            build job: 'RoboShop_WebApp/payment', parameters: [string(name: 'tfaction', value: "terraform plan")]
+            }
+            if (action == "destroy") {
+            build job: 'RoboShop_WebApp/payment', parameters: [string(name: 'tfaction', value: "terraform destory -auto-approve")]
+            }
+            }
+          }
+        }
+        stage ('Installing Shipping') {
+          steps {
+            script {
+            if (action == "apply") {
+            build job: 'RoboShop_WebApp/shipping', parameters: [string(name: 'tfaction', value: "terraform apply -auto-approve")]
+            }
+            if (action == "init") {
+            build job: 'RoboShop_WebApp/shipping', parameters: [string(name: 'tfaction', value: "terraform init")]
+            }
+            if (action == "plan") {
+            build job: 'RoboShop_WebApp/shipping', parameters: [string(name: 'tfaction', value: "terraform plan")]
+            }
+            if (action == "destroy") {
+            build job: 'RoboShop_WebApp/shipping', parameters: [string(name: 'tfaction', value: "terraform destory -auto-approve")]
+            }
+            }
+            }
+            }
+            } //parallel
+            } //stage
+    stage ('Installing Frontend') {
+        steps {
+            script {
+            if (action == "apply") {
+            build job: 'RoboShop_WebApp/frontend', parameters: [string(name: 'tfaction', value: "terraform apply -auto-approve")]
+            }
+            if (action == "init") {
+            build job: 'RoboShop_WebApp/frontend', parameters: [string(name: 'tfaction', value: "terraform init")]
+            }
+            if (action == "plan") {
+            build job: 'RoboShop_WebApp/frontend', parameters: [string(name: 'tfaction', value: "terraform plan")]
+            }
+            if (action == "destroy") {
+            build job: 'RoboShop_WebApp/frontend', parameters: [string(name: 'tfaction', value: "terraform destory -auto-approve")]
+            }
+            }
+          }
         }
     }
     }
