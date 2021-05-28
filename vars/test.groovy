d5b4386fddfd09e4663fc6a9e2b96cfde5ea9b4f
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-28 09:51:37 +0530

    roboshop

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,9 +1,8 @@
-def call (string action) {
-    if (action == "apply") {
+def call(String action) {
+  if (action == "apply") {
 
         pipeline {
             agent any 
-
             environment {
                 action = "${string.action}"
             }
