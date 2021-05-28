2b7c8b920855c245cbf4d278e5bc373ccea8fb5c
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-28 10:19:16 +0530

    roboshop

--- vars/test.groovy
+++ vars/test.groovy
@@ -4,7 +4,7 @@ def call(String action) {
         pipeline {
             agent any 
             environment {
-                envvar = "${action}"
+                envvar = "${var.action}"
             }
 
         stages {
