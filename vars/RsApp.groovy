c00c418093a43e03e6cb8370e0c99f020c3c6ac5
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-20 10:33:26 +0530

    shared library

--- vars/RsApp.groovy
+++ vars/RsApp.groovy
@@ -8,28 +8,28 @@ def call(String action) {
             stages ('Robosho App with Terrafor & Ansible') {
                 stage ('Terraform apply') {
                     steps {
-                        if (action == 'apply'){
+                        if (action == "apply"){
                         sh "terraform apply --auto-approve"
                     }    
                     }
                     }
                 stage ('Terraform init') {
                     steps {
-                        if (action == 'init'){
+                        if (action == "init"){
                         sh "Terraform init"
                     }
                     }
                     }
                 stage ('Terraform destroy') {
                     steps {
-                    if (action == 'destroy'){
+                    if (action == "destroy"){
                         sh "terraform destroy --auto-approve"
                     }
                     }
                     }
                 stage ('Terraform plan') {
                     steps {
-                        if (tfaction == 'plan') {
+                        if (action == "plan") {
                         sh "terraform plan"
                     }
                     }
