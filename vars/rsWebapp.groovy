929dbf785a11b2441e36d04f1302ba92b0108412
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-20 11:43:37 +0530

    shared library

--- vars/rsWebapp.groovy
+++ vars/rsWebapp.groovy
@@ -31,5 +31,10 @@ def call(String action) {
                     }
                 }
             }
+            post { 
+                always { 
+                    cleanWs()
+                }
+            }
         }
     }
