c14a8ee99f4b5213ac6e057e53de13e6194efa47
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-20 10:51:13 +0530

    shared library

--- vars/RsApp.groovy
+++ vars/RsApp.groovy
@@ -5,41 +5,11 @@ def call(String action) {
                 stage ('Share Library is working') {
                     script {
                     if (action == "plan") {
-                        sh "terraform init"
-                        sh "terrafrom plan"
-                        }
-                        }  
+                    sh "terraform init"
+                    sh "terrafrom plan"
+                    }
+                    }  
                     }
                 }
             }
         }
-
-
-// stage ('Terraform apply') {
-//                     steps {
-//                         if (action == "apply"){
-//                         sh "terraform apply --auto-approve"
-//                     }    
-//                     }
-//                     }
-//                 stage ('Terraform init') {
-//                     steps {
-//                         if (action == "init"){
-//                         sh "Terraform init"
-//                     }
-//                     }
-//                     }
-//                 stage ('Terraform destroy') {
-//                     steps {
-//                     if (action == "destroy"){
-//                         sh "terraform destroy --auto-approve"
-//                     }
-//                     }
-//                     }
-//                 stage ('Terraform plan') {
-//                     steps {
-//                         if (action == "plan") {
-//                         sh "terraform plan"
-//                     }
-//                     }
-//                     }
