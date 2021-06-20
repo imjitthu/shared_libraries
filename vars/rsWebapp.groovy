af98f6094801358a04d2ad1e4d4bf19f35fb8cd9
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-20 10:57:55 +0530

    shared library

--- vars/rsWebapp.groovy
+++ vars/rsWebapp.groovy
@@ -9,6 +9,21 @@ def call(String action) {
                     sh "terraform init"
                     sh "terraform plan"
                     }
+                    if (action == "init") {
+                    sh "terraform init"
+                    }
+                    if (action == "apply") {
+                    sh "terraform init"
+                    sh "terraform apply --auto-approve"
+                    }
+                    if (action == "destroy") {
+                    sh "terraform init"
+                    sh "terraform destroy --auto-approve"
+                    }
+                    if (action == "validate") {
+                    sh "terraform init"
+                    sh "terraform validate"
+                    }
                     }  
                     }
                 }
