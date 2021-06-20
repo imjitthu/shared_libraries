11576a83d5e6742821cb31f8d09a4037592e22fd
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-20 09:47:05 +0530

    shared library

--- vars/App.groovy
+++ vars/App.groovy
@@ -1,29 +1,29 @@
 def call(string tfaction) {
     pipeline {
         agent {
-            environment {
-                action = "${params.tfaction}"
-            }
+            // environment {
+            //     action = "${params.env.tfaction}"
+            // }
             stages {
                 stage ('Terraform apply') {
-                    when {expression {env.action == 'apply'}}
+                    when {expression {env.tfaction == 'apply'}}
                     steps {
                         sh "terraform apply --auto-approve"
                     }
                 stage ('Terraform init') {
-                    when {expression {env.action == 'init'}}
+                    when {expression {env.tfaction == 'init'}}
                     steps {
                         sh "Terraform init"
                     }
                     }
                 stage ('Terraform destroy') {
-                    when {expression {env.action == 'destroy'}}
+                    when {expression {env.tfaction == 'destroy'}}
                     steps {
                         sh "terraform destroy --auto-approve"
                     }
                     }
                 stage ('Terraform plan') {
-                    when {expression {env.action == 'plan'}}
+                    when {expression {env.tfaction == 'plan'}}
                     steps {
                         sh "terraform plan"
                     }
