40a612d0d9c99efec8cdcd7a56c49b82d14d87eb
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-20 10:48:44 +0530

    shared library

--- vars/RsApp.groovy
+++ vars/RsApp.groovy
@@ -5,34 +5,42 @@ def call(String action) {
         agent any
 
             stages {
-                stage ('Terraform apply') {
-                    steps {
-                        if (action == "apply"){
-                        sh "terraform apply --auto-approve"
-                    }    
+                stage ('Share Library is working') {
+                    if (action == "plan") {
+                        sh "terrafrom plan"
                     }
-                    }
-                stage ('Terraform init') {
-                    steps {
-                        if (action == "init"){
-                        sh "Terraform init"
-                    }
-                    }
-                    }
-                stage ('Terraform destroy') {
-                    steps {
-                    if (action == "destroy"){
-                        sh "terraform destroy --auto-approve"
-                    }
-                    }
-                    }
-                stage ('Terraform plan') {
-                    steps {
-                        if (action == "plan") {
-                        sh "terraform plan"
-                    }
-                    }
-                    }
+                }
+                
                 }
             }
         }
+
+
+// stage ('Terraform apply') {
+//                     steps {
+//                         if (action == "apply"){
+//                         sh "terraform apply --auto-approve"
+//                     }    
+//                     }
+//                     }
+//                 stage ('Terraform init') {
+//                     steps {
+//                         if (action == "init"){
+//                         sh "Terraform init"
+//                     }
+//                     }
+//                     }
+//                 stage ('Terraform destroy') {
+//                     steps {
+//                     if (action == "destroy"){
+//                         sh "terraform destroy --auto-approve"
+//                     }
+//                     }
+//                     }
+//                 stage ('Terraform plan') {
+//                     steps {
+//                         if (action == "plan") {
+//                         sh "terraform plan"
+//                     }
+//                     }
+//                     }
