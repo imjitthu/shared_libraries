861eb6883da36968c5bfae64da60f7d7076b5431
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-20 09:26:51 +0530

    app.groovy

--- vars/App.groovy
+++ vars/App.groovy
@@ -6,20 +6,24 @@ def call(string tfaction) {
             }
             stages {
                 stage ('Terraform apply') {
+                    when {expression {env.action == 'apply'}}
                     steps {
                         sh "terraform apply --auto-approve"
                     }
                 stage ('Terraform init') {
+                    when {expression {env.action == 'init'}}
                     steps {
                         sh "Terraform init"
                     }
                     }
                 stage ('Terraform destroy') {
+                    when {expression {env.action == 'destroy'}}
                     steps {
                         sh "terraform destroy --auto-approve"
                     }
                     }
                 stage ('Terraform plan') {
+                    when {expression {env.action == 'plan'}}
                     steps {
                         sh "terraform plan"
                     }
